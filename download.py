40abfdc737ee17b170c0d677fdd0df0264d07e73
Jacob <121278501+JacobZCodes@users.noreply.github.com>
2024-07-31 14:28:42 -0500

    Fixed get download dir func

--- download.py
+++ download.py
@@ -10,9 +10,9 @@ from pathlib import Path
 def get_download_directory():
     home = Path.home()
     if os.name == 'nt':  # Windows
-        return home / 'Downloads'
+        return str(home / 'Downloads')
     elif os.name == 'posix':  # Linux and MacOS
-        return home / 'Downloads'
+        return str(home / 'Downloads')
     else:
         raise NotImplementedError(f"Unsupported OS: {os.name}")
 
