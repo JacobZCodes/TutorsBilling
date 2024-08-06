b29f1f5bcc99b5aa86e4e1ae0d73580a926cc130
Jacob <121278501+JacobZCodes@users.noreply.github.com>
2024-08-06 14:11:52 -0500

    AWS database linked and code updates database. Working on survey send reminder

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+import psycopg2
+import credentials
 import download
 from CSV import findMostRecentCSV, csv_to_txt
 import dates
-from dates import convert_comma_date_to_slash_date, is_past_today
+from dates import convert_comma_date_to_slash_date, is_past_today, is_new_client
 from download import download_acuity_data
 import os
 from download import get_download_directory
@@ -14,7 +16,25 @@ invalid_meeting_types = ['Short Meeting', 'Meeting', 'Business Meeting', 'ACT Di
 destination = get_download_directory()
 download_directory = get_download_directory()
 
-def clean_df(df):
+dbname = credentials.db_name
+user = credentials.db_user
+password = credentials.db_pass# MOTHERFUCKA HIDE THIS SHIT!
+host = credentials.db_host
+port = '5432'
+
+conn_string = f"dbname={dbname} user={user} password={password} host={host} port={port}"
+
+def partial_clean_df(df): # remove people who are not getting tutored
+    indices_to_drop = []
+    for index, row in df.iterrows():
+        curr_date = convert_comma_date_to_slash_date((row['Start Time'].split()[0] + " " + row['Start Time'].split()[1] + row['Start Time'].split()[2]))
+        if row['Type'] in invalid_meeting_types: # remove people who are not getting tutored
+            indices_to_drop.append(index)
+        if is_past_today(curr_date, dates.today): # Acuity bug - grabs more dates than I want, so remove all rows whose dates are past today's date
+            indices_to_drop.append(index)
+    return df.drop(indices_to_drop)
+
+def clean_df(df): # remove people who have already paid and who are not getting tutored
     indices_to_drop = []
     for index, row in df.iterrows():
         curr_date = convert_comma_date_to_slash_date((row['Start Time'].split()[0] + " " + row['Start Time'].split()[1] + row['Start Time'].split()[2]))
@@ -29,13 +49,12 @@ def clean_df(df):
 def createBillingDict(df):
     billing = {}
     for index, row in df.iterrows():
-        fullName = row['Last Name'] + " " + row['First Name']
-        amountOwed = str(row['Appointment Price'])
-        sessionDate = dates.convert_comma_date_to_slash_date(row['Start Time'].split()[0] + " " + row['Start Time'].split()[1] + row['Start Time'].split()[2])
+        fullName = row['First Name'] + "_" + row['Last Name']
+        amountOwed = row['Appointment Price']
         if (fullName not in billing.keys()):
-            billing[fullName] = [[sessionDate, amountOwed]]
+            billing[fullName] = amountOwed
         else:
-            billing[fullName].append([sessionDate, amountOwed])
+            billing[fullName] += amountOwed
     return billing
 
 def getTotalOwed(billing):
@@ -55,32 +74,120 @@ def generateTxt(fileName, destination, names, billing, total): # pretty write bi
                 file.write(session[0] + " " + session[1] + "\n")
     return (rf"{destination}\{fileName}")
 
-def process_billing_csv():
-    download_acuity_data() # downloads billing CSV from Acuity
+def get_df_all():
     csv_path = findMostRecentCSV(download_directory) 
-    df_all = pd.read_csv(csv_path)
-    df_not_paid = clean_df(df_all)
-    billing = createBillingDict(df_not_paid)
-    # Sort names alphabetically
-    tempList = []
-    for key in billing.keys():
-        tempList.append(key)
-    sortedNames = sorted(tempList)
-    fileName = csv_to_txt(csv_path) # schedule.txt
-    total = getTotalOwed(billing)
-    content_path = generateTxt(fileName=fileName, destination=destination, names=sortedNames, billing=billing, total=total)
-    recepients = [] # TO DO - READ IN ENV VARS AS LIST
-    recepients.append(os.getenv("GMAIL_RECEPIENT_1"))
-    recepients.append(os.getenv("GMAIL_RECEPIENT_2"))
-    recepients.append(os.getenv("GMAIL_RECEPIENT_3"))
-    email_pass = os.getenv("GMAIL_PASS")
-    sender = os.getenv("GMAIL_SEND_ADDRESS")
-    send_file(email_pass=email_pass, sender=sender, recepients=recepients, content_path=content_path)
+    return pd.read_csv(csv_path)
+
+def createDatesDict(df_partial_clean):
+    dateDict = {}
+    for index, row in df_partial_clean.iterrows():
+        firstName = row['First Name']
+        lastName = row['Last Name']
+        fullName = firstName + "_" + lastName
+        if (fullName not in dateDict.keys()):
+            dateDict[fullName] = [convert_comma_date_to_slash_date((row['Start Time'].split()[0] + " " + row['Start Time'].split()[1] + row['Start Time'].split()[2]))]
+        else:
+            dateDict[fullName].append(convert_comma_date_to_slash_date((row['Start Time'].split()[0] + " " + row['Start Time'].split()[1] + row['Start Time'].split()[2])))
+    # print it out
+    for key in dateDict.keys():
+        print(key, dateDict[key])
+    return dateDict
+        
+
+# Populates a first,last,email columns and returns a list of client first last names
+def populate_first_last_email(df_partial_clean):
+    clientNames = []
+    # download_acuity_data() # downloads billing CSV from Acuity
+    conn = psycopg2.connect(conn_string)
+    curr = conn.cursor()    
 
 
-    path = rf"{destination}\{fileName}"
+    for index, row in df_partial_clean.iterrows(): # Populate first name, last name, and email
+        print(index)
+        firstName = row['First Name']
+        lastName = row['Last Name']
+        fullName = firstName + "_" + lastName
+        if fullName in clientNames:
+            pass
+        else:
+            clientNames.append(fullName)
+        email = row['Email']
+
+        # Convert email NaN to empty string
+        if pd.isna(email):
+            email = ''
+
+        # Check if record already exists -- skip over it if it does
+        curr.execute("""SELECT 1 FROM clients WHERE firstName=%s AND lastName = %s;""", (firstName,lastName))
+        exists = curr.fetchone()       
+        if exists:
+            continue
+        curr.execute(f"""INSERT INTO clients (firstName, lastName, email) VALUES (%s,%s,%s)""", (firstName, lastName, email))
+        
+
+    conn.commit()
+
+    curr.close()
+    conn.close()
+    return clientNames
+
+def populate_owes(df_all):
+    conn = psycopg2.connect(conn_string)
+    curr = conn.cursor()    
+    df_clean = clean_df(df_all) # Create a separate df whose members include only people who OWE money
+    billing = createBillingDict(df_clean)
+    for key in billing.keys(): # Add to owes column
+        print(key)
+        lastName = key.split("_")[1]
+        firstName = key.split("_")[0]
+        curr.execute(f"""
+        UPDATE clients
+        SET owes = %s
+        WHERE firstName = %s AND lastName = %s;
+        """, (billing[key], firstName, lastName))
+    conn.commit()
+
+    curr.close()
+    conn.close()
+
+def populate_startdate(datesDict):
+    conn = psycopg2.connect(conn_string)
+    curr = conn.cursor()    
+    # code goes here
+    for key in datesDict.keys():
+        firstName = key.split("_")[0]
+        lastName = key.split("_")[1]
+        startDate = datesDict[key][0]
+        print(startDate)
+        curr.execute("""UPDATE clients SET startdate = %s WHERE firstname = %s AND lastname = %s""", (startDate, firstName, lastName))
     
-    return path
+    conn.commit()
+
+    curr.close()
+    conn.close()
+
+def check_for_new_clients():
+    conn = psycopg2.connect(conn_string)
+    curr = conn.cursor()
+    
+    curr.execute("""SELECT * FROM clients;""")
+    records = curr.fetchall()
+    for record in records:
+        if is_new_client(record[5], dates.today) and record[6] == False: # Client needs to get a survey sent to them
+            
+
+    # conn.commit()
+
+    curr.close()
+    conn.close()
+
+def update_database():
+    df_all = get_df_all()
+    df_partial_clean = partial_clean_df(df_all)
+    populate_first_last_email(df_partial_clean)
+    populate_owes(df_all)
+    populate_startdate(createDatesDict(df_partial_clean))
 
 if __name__ == "__main__":
-    process_billing_csv()
+
+    check_for_new_clients()
