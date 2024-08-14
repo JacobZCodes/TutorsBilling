5b349e540e05612e49ee16e710527e9c859be786
Jacob <121278501+JacobZCodes@users.noreply.github.com>
2024-08-14 13:11:58 -0500

    Debugging

--- main.py
+++ main.py
@@ -36,28 +36,32 @@ def send_survey():
     curr = conn.cursor()       
     curr.execute("SELECT * FROM clients WHERE isnewclient = TRUE AND receivedsurvey = FALSE;")
     rows = curr.fetchall()
-    email = {}
-
     # REMOVE SHOWING WHO IS VALID RECEPIENT
     for row in rows:
         print(row)
+    # POPULATE EMAIL DICT 
+    addresses = {}
+    for row in rows:
+        name = row[0] + "_" + row[1]
+        email_address = row[2]
+        addresses[name] = email_address
     # REMOVE PRUNING OF FARIS
     namesToRemove = []
-    for name in email.keys():
+    for name in addresses.keys():
         if name != "***REMOVED***":
             namesToRemove.append(name)
     for name in namesToRemove:
-        del email[name]
+        del addresses[name]
     print("printing pruned emails..\n")
-    print(email)
+    print(addresses)
 
     # REMOVE - ADD STAFF RECEPIENTS FOR TESTING
     recepients = []
     recepients.append(os.getenv("GMAIL_RECEPIENT_0"))
 
     # KEEP - ADD CLIENT RECEPIENTS
-    for recepient in email.keys():
-        recepients.append(email[recepient])
+    for recepient in addresses.keys():
+        recepients.append(addresses[recepient])
 
     # REMOVE - SHOW RECEPIENTS
     print("Recepients:")
@@ -67,10 +71,10 @@ def send_survey():
     email_pass = os.getenv("GMAIL_SURVEY_SENDER_PASS")
     sender = os.getenv("GMAIL_SURVEY_SENDER")
 
-    send_file(email_pass=email_pass, sender=sender, recepients=recepients, content_path=generateTxt(get_download_directory()))
+    # send_file(email_pass=email_pass, sender=sender, recepients=recepients, content_path=generateTxt(get_download_directory()))
 
     # CHANGE RECEIVEDSURVEY TO BE TRUE
-    for recepient in email.keys():
+    for recepient in addresses.keys():
         firstName = recepient.split("_")[0]
         lastName = recepient.split("_")[1]
         curr.execute(f"""
